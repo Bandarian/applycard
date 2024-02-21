1c94ec1ff1ba489f3d58053640a93e7bc3fe5a2f
Bandarian <64462886+Bandarian@users.noreply.github.com>
2024-02-21 18:21:06 +1100

    Update appCardDraft.js

--- appCardDraft.js
+++ appCardDraft.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+y$(document).ready(function () {
   const applyCardOdo = document.getElementById("applyCardOdo");
   if (!applyCardOdo) {
     console.log("Element with ID 'applyCardOdo' not found. Aborting script.");
@@ -16,7 +16,7 @@ function animateStepZero(stepZero) {
   const stepZeroTl = gsap.timeline({
     ease: "power2.inOut",
   });
-   stepZeroTl.fromTo(odoWrapGsap, { width: 0 }, { width: "100%", duration: 1, ease: "power1.out",});
+   stepZeroTl.fromTo(odoWrapGsap, { width: 0 }, { width: "100%", duration: 1, ease: "power1.out", delay:2});
 
   const odometer = new Odometer({
     el: odometerElement,
